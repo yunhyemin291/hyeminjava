6370141e23ee6f2edf9895be915adcd1843a2527
yunhyemin <yunhyemin15@gmail.com>
2020-07-06 17:10:43 +0900

    53일차 수정

--- src/day53/Ex5.java
+++ src/day53/Ex5.java
@@ -6,9 +6,18 @@ public class Ex5 {
 		// TODO Auto-generated method stub
 		int sum=0;
 		for(int i=1;i<=10;i++) {
-			if(i%2==0) {
-				sum+=i;
+			if(i%2==1) {
+				continue;				
 			}
+			sum+=i;
+		}
+		System.out.println(sum);
+		int j=0;
+		sum=0;
+		while(++j<=10) {
+			if(j%2==1)
+				continue;
+			sum+=j;
 		}
 		System.out.println(sum);
 	}
